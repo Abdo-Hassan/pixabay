64cfb53d9f2c99016d6958d8928f020ffe9e8f92
abdo hassan <abdocoder0@gmail.com>
2019-08-23 16:57:40 +0200

    setting the search

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,12 +1,21 @@
 import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import axios from 'axios';
 
 const useStyles = makeStyles(theme => ({
+  container: {
+    margin: 10
+  },
   textField: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    width: 200
+    marginBottom: theme.spacing(2)
+  },
+  select: {
+    width: 150
   }
 }));
 
@@ -19,10 +28,10 @@ const Search = () => {
     apiKey: '10853125-b01d14bd9653fb17a1bdb50b6',
     images: []
   });
-  const { searchText } = search;
-  console.log(searchText);
+  const { searchText, amount, apiUrl, apiKey, images } = search;
+  console.log(amount);
   return (
-    <div>
+    <div className={classes.container}>
       <TextField
         id='standard-name'
         label='Name'
@@ -30,7 +39,29 @@ const Search = () => {
         value={searchText}
         onChange={e => setSearch({ searchText: e.target.value })}
         margin='normal'
+        autoFocus
+        fullWidth
       />
+      <div>
+        <FormControl className={classes.formControl}>
+          <InputLabel htmlFor='amount-simple'>Amount</InputLabel>
+          <Select
+            className={classes.select}
+            value={amount}
+            onChange={e => setSearch({ amount: e.target.value })}
+            inputProps={{
+              name: 'amount',
+              id: 'amount-simple'
+            }}
+          >
+            <MenuItem value={5}>5</MenuItem>
+            <MenuItem value={10}>10</MenuItem>
+            <MenuItem value={15}>15</MenuItem>
+            <MenuItem value={30}>30</MenuItem>
+            <MenuItem value={50}>50</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 };

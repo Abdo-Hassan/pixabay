ffb3499fb97d73d5df148022b3dc67b6403a6b70
abdo hassan <abdocoder0@gmail.com>
2019-08-24 16:21:45 +0200

    search photos Done ✔

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -4,6 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
+import Button from '@material-ui/core/Button';
 import Select from '@material-ui/core/Select';
 import axios from 'axios';
 
@@ -24,20 +25,35 @@ const Search = () => {
   const [search, setSearch] = useState({
     searchText: '',
     amount: '',
-    apiUrl: 'https://pixabay/com/api',
+    apiUrl: 'https://pixabay.com/api',
     apiKey: '10853125-b01d14bd9653fb17a1bdb50b6',
     images: []
   });
+
   const { searchText, amount, apiUrl, apiKey, images } = search;
-  console.log(amount);
+  const onTextChange = e => {
+    setSearch({ ...search, [e.target.name]: e.target.value });
+  };
+  const searchPhotos = () => {
+    axios
+      .get(
+        `${apiUrl}/?key=${apiKey}&q=${searchText}&image_type=photo&per_page=${amount}&safesearch=true`
+      )
+      .then(res => {
+        setSearch({ ...search, images: res.data.hits });
+        console.log(images);
+      })
+      .catch(err => console.log(err));
+  };
   return (
     <div className={classes.container}>
       <TextField
         id='standard-name'
-        label='Name'
         className={classes.textField}
+        placeholder='Search...'
         value={searchText}
-        onChange={e => setSearch({ searchText: e.target.value })}
+        name='searchText'
+        onChange={onTextChange}
         margin='normal'
         autoFocus
         fullWidth
@@ -48,7 +64,7 @@ const Search = () => {
           <Select
             className={classes.select}
             value={amount}
-            onChange={e => setSearch({ amount: e.target.value })}
+            onChange={e => setSearch({ ...search, amount: e.target.value })}
             inputProps={{
               name: 'amount',
               id: 'amount-simple'
@@ -62,6 +78,14 @@ const Search = () => {
           </Select>
         </FormControl>
       </div>
+      <Button
+        variant='contained'
+        color='primary'
+        style={{ marginTop: 20 }}
+        onClick={searchPhotos}
+      >
+        Search
+      </Button>
     </div>
   );
 };

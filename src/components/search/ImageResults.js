992bbcdbf0b0e75d1761c15cae6f5e6f91fe9211
abdo hassan <abdocoder0@gmail.com>
2019-08-24 17:49:02 +0200

    get images results and display in buttons

---
+++ src/components/search/ImageResults.js
@@ -0,0 +1,133 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import ButtonBase from '@material-ui/core/ButtonBase';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    minWidth: 300,
+    width: '100%'
+  },
+  image: {
+    position: 'relative',
+    height: 200,
+    [theme.breakpoints.down('xs')]: {
+      width: '100% !important', // Overrides inline-style
+      height: 100
+    },
+    '&:hover, &$focusVisible': {
+      zIndex: 1,
+      '& $imageBackdrop': {
+        opacity: 0.15
+      },
+      '& $imageMarked': {
+        opacity: 0
+      },
+      '& $imageTitle': {
+        border: '4px solid currentColor'
+      }
+    }
+  },
+  focusVisible: {},
+  imageButton: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: theme.palette.common.white
+  },
+  imageSrc: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center 40%'
+  },
+  imageBackdrop: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundColor: theme.palette.common.black,
+    opacity: 0.4,
+    transition: theme.transitions.create('opacity')
+  },
+  imageTitle: {
+    position: 'relative',
+    padding: `${theme.spacing(2)}px ${theme.spacing(4)}px ${theme.spacing(1) +
+      6}px`
+  },
+  imageMarked: {
+    height: 3,
+    width: 18,
+    backgroundColor: theme.palette.common.white,
+    position: 'absolute',
+    bottom: -2,
+    left: 'calc(50% - 9px)',
+    transition: theme.transitions.create('opacity')
+  }
+}));
+const ImageResults = ({ images }) => {
+  const classes = useStyles();
+
+  let imageListContent;
+  if (images) {
+    imageListContent = (
+      <Fragment>
+        <Grid container>
+          {images.map(image => (
+            <Grid item md={4}>
+              <ButtonBase
+                focusRipple
+                key={image.id}
+                className={classes.image}
+                focusVisibleClassName={classes.focusVisible}
+                style={{
+                  width: '100%'
+                }}
+              >
+                <span
+                  className={classes.imageSrc}
+                  style={{
+                    backgroundImage: `url(${image.largeImageURL})`
+                  }}
+                />
+                <span className={classes.imageBackdrop} />
+                <span className={classes.imageButton}>
+                  <Typography
+                    component='span'
+                    variant='subtitle1'
+                    color='inherit'
+                    className={classes.imageTitle}
+                  >
+                    {image.tags}
+                    <span className={classes.imageMarked} />
+                  </Typography>
+                </span>
+              </ButtonBase>
+            </Grid>
+          ))}
+        </Grid>
+      </Fragment>
+    );
+  } else {
+    imageListContent = <p>no images here</p>;
+  }
+  return <div>{imageListContent}</div>;
+};
+
+ImageResults.propTypes = {
+  images: PropTypes.array.isRequired
+};
+export default ImageResults;
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -7,6 +7,8 @@ import FormControl from '@material-ui/core/FormControl';
 import Button from '@material-ui/core/Button';
 import Select from '@material-ui/core/Select';
 import axios from 'axios';
+import ImageResults from './ImageResults';
+import { Typography } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -31,9 +33,7 @@ const Search = () => {
   });
 
   const { searchText, amount, apiUrl, apiKey, images } = search;
-  const onTextChange = e => {
-    setSearch({ ...search, [e.target.name]: e.target.value });
-  };
+
   const searchPhotos = () => {
     axios
       .get(
@@ -41,51 +41,59 @@ const Search = () => {
       )
       .then(res => {
         setSearch({ ...search, images: res.data.hits });
-        console.log(images);
       })
       .catch(err => console.log(err));
   };
+
   return (
-    <div className={classes.container}>
-      <TextField
-        id='standard-name'
-        className={classes.textField}
-        placeholder='Search...'
-        value={searchText}
-        name='searchText'
-        onChange={onTextChange}
-        margin='normal'
-        autoFocus
-        fullWidth
-      />
-      <div>
-        <FormControl className={classes.formControl}>
-          <InputLabel htmlFor='amount-simple'>Amount</InputLabel>
-          <Select
-            className={classes.select}
-            value={amount}
-            onChange={e => setSearch({ ...search, amount: e.target.value })}
-            inputProps={{
-              name: 'amount',
-              id: 'amount-simple'
-            }}
-          >
-            <MenuItem value={5}>5</MenuItem>
-            <MenuItem value={10}>10</MenuItem>
-            <MenuItem value={15}>15</MenuItem>
-            <MenuItem value={30}>30</MenuItem>
-            <MenuItem value={50}>50</MenuItem>
-          </Select>
-        </FormControl>
+    <div>
+      <div className={classes.container}>
+        <TextField
+          id='standard-name'
+          className={classes.textField}
+          placeholder='Search...'
+          value={searchText}
+          name='searchText'
+          onChange={e =>
+            setSearch({ ...search, [e.target.name]: e.target.value })
+          }
+          margin='normal'
+          autoFocus
+          fullWidth
+        />
+        <div>
+          <FormControl className={classes.formControl}>
+            <InputLabel htmlFor='amount-simple'>Amount</InputLabel>
+            <Select
+              className={classes.select}
+              value={amount}
+              onChange={e => setSearch({ ...search, amount: e.target.value })}
+              inputProps={{
+                name: 'amount',
+                id: 'amount-simple'
+              }}
+            >
+              <MenuItem value={5}>5</MenuItem>
+              <MenuItem value={10}>10</MenuItem>
+              <MenuItem value={15}>15</MenuItem>
+              <MenuItem value={30}>30</MenuItem>
+              <MenuItem value={50}>50</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+        <Button
+          variant='contained'
+          color='primary'
+          style={{ margin: '20px 0' }}
+          onClick={searchPhotos}
+        >
+          Search
+        </Button>
+        <Typography variant='h3' gutterBottom>
+          Images For {images.tags}
+        </Typography>
       </div>
-      <Button
-        variant='contained'
-        color='primary'
-        style={{ marginTop: 20 }}
-        onClick={searchPhotos}
-      >
-        Search
-      </Button>
+      {images.length > 0 ? <ImageResults images={images} /> : null}
     </div>
   );
 };
